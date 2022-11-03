4f8cde7b7464b625cbb08e28a16e3878b19e5980
atai <imankanov2002@gmail.com>
2022-11-04 02:26:15 +0600

    first commit created mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace homework_64_Atai.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
